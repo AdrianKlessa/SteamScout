161683b1596299d98ed86763a431203297d4c5b8
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-07-16 21:24:27 +0200

    Recommender, part 1

--- src/recommender_ensemble.py
+++ src/recommender_ensemble.py
@@ -1,4 +1,6 @@
 from src.Game import Game
+import numpy as np
+from numpy.linalg import norm
 
 
 class RecommendationResult:
@@ -10,3 +12,29 @@ class RecommendationResult:
     @property
     def review_score(self) -> float:
         return self.game.fraction_positive_reviews
+
+
+def cosine_similarity(A, B):
+    all_zeros = not (np.any(A) and np.any(B))
+    if all_zeros:
+        return 0.0
+    return (np.dot(A, B) / (norm(A) * norm(B)))
+
+
+def games_to_result(searched_game: Game, found_game: Game):
+    doc2vec1 = searched_game.description_vector
+    doc2vec2 = found_game.description_vector
+
+    tags_1 = searched_game.tags_vector
+    tags_2 = found_game.tags_vector
+
+    doc2vec_score = cosine_similarity(doc2vec1, doc2vec2)
+    tags_score = cosine_similarity(tags_1, tags_2)
+
+    return RecommendationResult(found_game, doc2vec_score, tags_score)
+
+def get_similar_games_by_description(game: Game):
+    pass
+
+def get_similar_games_by_tags(game : Game):
+    pass
--- src/vectordb_manager.py
+++ src/vectordb_manager.py
@@ -15,6 +15,7 @@ class GameTagDoc(BaseDoc):
     game_id: int = 0
     embedding: NdArray[448]
 
+
 DESCRIPTION_DB_DIR = Path(__file__).resolve().parent / "databases" / "game_description_db"
 TAG_DB_DIR = Path(__file__).resolve().parent / "databases" / "game_tag_db"
 
@@ -22,19 +23,32 @@ description_db = HNSWVectorDB[GameDescriptionDoc](workspace=DESCRIPTION_DB_DIR, 
 tags_db = HNSWVectorDB[GameTagDoc](workspace=TAG_DB_DIR, space="cosine")
 
 
-def find_similar_by_description_vector(description_vector, no_results=10):
+def app_ids_from_results(res_object, ignore_first=True):
+    ids = []
+    if ignore_first:
+        start = 1
+    else:
+        start = 0
+    for m in res_object[0].matches[start:]:  # First item will be exact game you looked for
+        ids.append(m.game_id)
+    return ids
+
+
+def find_similar_by_description_vector(description_vector, no_results=10, ignore_first=True):
     query = GameDescriptionDoc(text='query', embedding=description_vector)
     results = description_db.search(inputs=DocList[GameDescriptionDoc]([query]), limit=no_results)
-    return results
+    return app_ids_from_results(results)
 
-def find_similar_by_tags_vector(tags_vector, no_results=10):
+
+def find_similar_by_tags_vector(tags_vector, no_results=10, ignore_first=True):
     query = GameTagDoc(text='query', embedding=tags_vector)
     results = tags_db.search(inputs=DocList[GameTagDoc]([query]), limit=no_results)
-    return results
+    return app_ids_from_results(results)
+
 
 if __name__ == '__main__':
-    cs_go_tags_vector = sqlite_manager.get_games_by_app_id(730)[0]["tags_vector"]
-    res = find_similar_by_tags_vector(cs_go_tags_vector)
-    for m in res[0].matches[1:]:
-        match_game_id = m.game_id
-        print(sqlite_manager.get_games_by_app_id(match_game_id)[0]["game_name"])
+    cs_go_tags_vector = sqlite_manager.get_games_by_app_id(730)[0].tags_vector
+    res = find_similar_by_tags_vector(cs_go_tags_vector, 2)
+    for i in res:
+
+        print(sqlite_manager.get_games_by_app_id(i)[0].game_name)
