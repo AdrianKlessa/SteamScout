d8962c222731ce371414c0dc3b99b1c072bff553
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-07-17 16:57:04 +0200

    Case-insensitive search

--- src/sqlite_manager.py
+++ src/sqlite_manager.py
@@ -51,9 +51,9 @@ def get_games_by_app_id(app_id: int) -> Iterable[Game]:
 
 def get_games_by_name(name: str, exact=True) -> Iterable[Game]:
     if exact:
-        statement = f"SELECT app_id,game_name,description,tags,positive_reviews,negative_reviews,description_vector,tags_vector FROM games WHERE game_name = '{name}'"
+        statement = f"SELECT app_id,game_name,description,tags,positive_reviews,negative_reviews,description_vector,tags_vector FROM games WHERE LOWER(game_name) = '{name.lower()}'"
     else:
-        statement = f"SELECT app_id,game_name,description,tags,positive_reviews,negative_reviews,description_vector,tags_vector FROM games WHERE game_name LIKE '%{name}%'"
+        statement = f"SELECT app_id,game_name,description,tags,positive_reviews,negative_reviews,description_vector,tags_vector FROM games WHERE LOWER(game_name) LIKE '%{name.lower()}%'"
     try:
         with sqlite3.connect(SQLITE_PATH, detect_types=sqlite3.PARSE_DECLTYPES) as conn:
             cursor = conn.cursor()
@@ -74,7 +74,10 @@ def results_to_game(result_list):
 
 
 if __name__ == "__main__":
-    print(get_games_by_app_id(10)[0].fraction_positive_reviews)
+    #print(get_games_by_app_id(10)[0].fraction_positive_reviews)
+    #print(get_games_by_name("Counter-Strike")[0].fraction_positive_reviews)
+    print(get_games_by_name("counter-strike"))
+    print(get_games_by_app_id(10)[0])
     #print(results_to_dict(get_games_by_app_id(10)))
     #print(results_to_dict(get_games_by_app_id(10))[0]["description_vector"])
     #print(get_games_by_name("Counter-Strike", exact=False))
