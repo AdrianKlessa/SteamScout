a184e2e0366bd6d5ac14b9ac465e6d08d71ea2c5
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-07-17 22:59:14 +0200

    UI/UX improvement

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -4,8 +4,6 @@ import recommender_ensemble
 import pandas as pd
 
 
-
-
 def game_search(text):
     found_names = [game.game_name for game in sqlite_manager.get_games_by_name(text, exact=False)]
     print(found_names)
@@ -33,18 +31,24 @@ def find_similar_games(text):
 
 with gr.Blocks() as app:
     gr.Markdown("### Similar Games search")
-    gr.Markdown("Find a game by writing its name in the search box, then pressing enter.")
-    gr.Markdown("After choosing from one of the found games in the dropdown, click 'FIND SIMILAR'")
+    gr.Markdown("Find a game by writing its name in the search box, then pressing 'SEARCH'.")
+    gr.Markdown(
+        "After choosing from one of the found games in the dropdown, click 'FIND SIMILAR' to get recommendations.")
+
     with gr.Row():
-        input_text = gr.Textbox(placeholder="Game name (press enter to search)",
-                                label="Game search by name (press enter to search)")
+        input_text = gr.Textbox(placeholder="Game name",
+                                label="Game search by name")
+        search_button = gr.Button(value="SEARCH")
+
     with gr.Row():
         input_dropdown = gr.Dropdown(choices=[], interactive=True, label="Select")
         select_button = gr.Button(value="FIND SIMILAR")
+
     output_dataframe = gr.Dataframe(label="Similar games:",
                                     value=pd.DataFrame(),
-                                    datatype=["markdown", "markdown", "markdown","markdown","html"])
+                                    datatype=["markdown", "markdown", "markdown", "markdown", "html"])
 
+    search_button.click(fn=game_change, inputs=[input_text], outputs=[input_dropdown])
     input_text.submit(fn=game_change, inputs=[input_text], outputs=[input_dropdown])
     select_button.click(fn=find_similar_games, inputs=[input_dropdown], outputs=[output_dataframe])
 
