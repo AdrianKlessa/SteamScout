3ecbc2f0c135fa59bcd73806c6f600372df5bb56
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-07-23 16:22:38 +0200

    Show tags when giving the user recommendations

--- src/html_elements.py
+++ src/html_elements.py
@@ -4,22 +4,37 @@ from recommender_ensemble import RecommendationResult
 
 
 def get_elements_from_recommendation(rec: RecommendationResult) -> Iterable[Any]:
-    return [i(rec) for i in (get_game_name, get_game_score, get_game_link, get_description_score, get_tags_score, get_review_score)]
+    return [i(rec) for i in
+            (get_summary, get_game_score, get_game_link, get_description_score, get_tags_score, get_review_score)]
 
-def get_game_name(rec: RecommendationResult) -> str:
-    return f'<b>{rec.game.game_name}</b>'
+
+def get_summary(rec: RecommendationResult) -> str:
+    return f'<b>{rec.game.game_name}</b><br>Tags: {get_tags_string(rec)}'
+
 
 def get_game_score(rec: RecommendationResult) -> str:
-    return f'<span style="color: rgb{str(score_color.get_color_for_score(rec.overall_score))}">&#9632; </span>{"{:.2f}".format(rec.overall_score*100)}'
+    return f'<span style="color: rgb{str(score_color.get_color_for_score(rec.overall_score))}">&#9632; </span>{"{:.2f}".format(rec.overall_score * 100)}'
+
 
 def get_game_link(rec: RecommendationResult) -> str:
     return f'<a href="{rec.game.game_link}" style="color: #0000EE;" target="_blank" rel="noopener noreferrer">{rec.game.game_link}</a>'
 
+
 def get_description_score(rec: RecommendationResult) -> str:
-    return f'<span style="color: rgb{str(score_color.get_color_for_score(rec.description_similarity))}">&#9632; </span>{"{:.2f}".format(rec.description_similarity*100)}'
+    return f'<span style="color: rgb{str(score_color.get_color_for_score(rec.description_similarity))}">&#9632; </span>{"{:.2f}".format(rec.description_similarity * 100)}'
+
 
 def get_tags_score(rec: RecommendationResult) -> str:
-    return f'<span style="color: rgb{str(score_color.get_color_for_score(rec.tags_similarity))}">&#9632; </span>{"{:.2f}".format(rec.tags_similarity*100)}'
+    return f'<span style="color: rgb{str(score_color.get_color_for_score(rec.tags_similarity))}">&#9632; </span>{"{:.2f}".format(rec.tags_similarity * 100)}'
+
 
 def get_review_score(rec: RecommendationResult) -> str:
-    return f'<span style="color: rgb{str(score_color.get_color_for_score(rec.review_score))}">&#9632; </span>{"{:.2f}".format(rec.review_score*100)}'
+    return f'<span style="color: rgb{str(score_color.get_color_for_score(rec.review_score))}">&#9632; </span>{"{:.2f}".format(rec.review_score * 100)}'
+
+
+def get_tags_string(rec: RecommendationResult) -> str:
+    tags = rec.game.tags.split(",")
+    tags_string = ", ".join(tags[:5])
+    if tags_string.lower().strip() == "nan":
+        return "N/A"
+    return tags_string
