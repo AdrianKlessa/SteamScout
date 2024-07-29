b183c59ca5b1d5fad796c4a9434fc59fbbf0c315
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-07-29 14:51:15 +0200

    Test fixes

--- tests/test_game_filtering.py
+++ tests/test_game_filtering.py
@@ -1,45 +1,46 @@
-from src import game_filtering
+from src.game_filtering import include_tag, exclude_tag
 from src.Game import Game
+from src.recommender_ensemble import RecommendationResult
 import unittest
 
 
 class TestTagsFiltering(unittest.TestCase):
-    game1 = Game(0,
-                 "Racing game",
-                 "a racing game",
-                 "Racing,Sports,Driving",
-                 10,
-                 10,
-                 None,
-                 None)
+    game1 = RecommendationResult(Game(0,
+                                      "Racing game",
+                                      "a racing game",
+                                      "Racing,Sports,Driving",
+                                      10,
+                                      10,
+                                      None,
+                                      None), 0, 0, 0)
 
-    game2 = Game(1,
-                 "WW2 FPS",
-                 "A WW2 shooter",
-                 "FPS,World War,Shooter",
-                 10,
-                 10,
-                 None,
-                 None)
-    game3 = Game(2,
-                 "WW3 FPS",
-                 "A WW3 shooter",
-                 "FPS,World War,Shooter,Futuristic",
-                 10,
-                 10,
-                 None,
-                 None)
+    game2 = RecommendationResult(Game(1,
+                                      "WW2 FPS",
+                                      "A WW2 shooter",
+                                      "FPS,World War,Shooter",
+                                      10,
+                                      10,
+                                      None,
+                                      None), 0, 0, 0)
+    game3 = RecommendationResult(Game(2,
+                                      "WW3 FPS",
+                                      "A WW3 shooter",
+                                      "FPS,World War,Shooter,Futuristic",
+                                      10,
+                                      10,
+                                      None,
+                                      None), 0, 0, 0)
 
     def test_include_tags(self):
         games = [self.game1, self.game2, self.game3]
-        filtered = game_filtering.include_tag(games, "World War")
+        filtered = include_tag(games, "World War")
         self.assertNotIn(self.game1, filtered)
         self.assertIn(self.game2, filtered)
         self.assertIn(self.game3, filtered)
 
     def test_exclude_tags(self):
         games = [self.game1, self.game2, self.game3]
-        filtered = game_filtering.exclude_tag(games, "FPS")
+        filtered = exclude_tag(games, "FPS")
         self.assertIn(self.game1, filtered)
         self.assertNotIn(self.game2, filtered)
         self.assertNotIn(self.game3, filtered)
---
+++ tests/test_score_color.py
@@ -0,0 +1,14 @@
+import unittest
+from src.score_color import get_color_for_score
+
+
+class TestScoreColor(unittest.TestCase):
+    def test_score_zero(self):
+        score = 0.0
+        expected_color = (255, 0, 0)
+        self.assertEqual(expected_color, get_color_for_score(score))
+
+    def test_score_one(self):
+        score = 1.0
+        expected_color = (0, 255, 0)
+        self.assertEqual(expected_color, get_color_for_score(score))
