29fbb765d93b082def5beb6420f13da3a021c35c
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2024-09-13 23:57:20 +0200

    Add comments in docker_prepare_data.py

--- src/scripts/docker_prepare_data.py
+++ src/scripts/docker_prepare_data.py
@@ -7,6 +7,8 @@ from gensim.models import doc2vec
 from gensim.models.doc2vec import TaggedDocument
 import numpy as np
 
+# Define file paths
+
 data_dir = Path(__file__).resolve().parents[2] / "data"
 model_path = str(Path(__file__).resolve().parents[2] / "models" / "doc2vec_trained")
 csv_path = data_dir / "raw" / "games.csv"
@@ -14,6 +16,8 @@ json_path = data_dir / "raw" / "games.json"
 pickle_path = data_dir / "processed" / "games_with_vectors.pickle"
 
 def convert_tags_from_dict(tags_dict):
+    # Used for converting the .json data into the format used in the .csv file
+    # i.e. dict of "Tag: tag_id" into string with comma-separated tags
     if len(tags_dict) == 0:
         return np.NaN
     key_list = list(tags_dict.keys())
@@ -40,6 +44,7 @@ def read_json_dataset():
 #df = pd.read_csv(csv_path)
 df = read_json_dataset()
 
+# Get games with English descriptions and train doc2vec on them
 df.dropna(subset=['Name', 'About the game'], how='any', inplace=True)
 english_descriptions = df[df['Supported languages'].str.contains("English")]
 
@@ -67,7 +72,6 @@ model.build_vocab(train_corpus)
 model.train(train_corpus, total_examples=model.corpus_count,
             epochs=model.epochs)  # Longer training than default because the dataset isn't large
 
-# Adding vectors
 df = read_json_dataset()
 model.save(model_path)
 
@@ -89,7 +93,7 @@ def desc_to_vector(desc: str) -> np.array:
 def str_tags_to_set(tags: str) -> Iterable[str]:
     return set(tags.split(","))
 
-
+# Get rows with non-empty tags and generate the tags dictionary (single tag string --> index in the embedded tag vector)
 tagged_games = df[df["Tags"].notnull()].copy()
 tagged_games["Tags_set"] = tagged_games["Tags"].apply(lambda x: str_tags_to_set(x))
 tags_set_list = tagged_games["Tags_set"].tolist()
@@ -114,11 +118,12 @@ def vectorize_str_tags(tags: str):
         vec[tag_dict[tag]] = 1
     return vec
 
-
+# Final pass - vectorize both the tags and descriptions, add as separate columns to the dataframe
 df["About the game"] = df["About the game"].astype(str)
 df["Tags"] = df["Tags"].astype(str)
 
 df["Description_vector"] = df["About the game"].apply(desc_to_vector)
 df["Tags_vector"] = df["Tags"].apply(vectorize_str_tags)
 
+# Pickle and use the pickle in later steps to avoid headaches with numpy arrays in .csv file
 df.to_pickle(pickle_path)
